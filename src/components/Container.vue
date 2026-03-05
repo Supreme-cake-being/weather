e98988c5fdad852080e7d16bd053373a59375286
Kirill Korchakovskyi <121553762+Supreme-cake-being@users.noreply.github.com>
2026-03-05 22:24:36 +0200

    added container

---
+++ src/components/Container.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 16px;
+}
+
+@media (min-width: 576px) {
+  .container {
+    max-width: 540px;
+    padding: 0 24px;
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1200px;
+    padding: 0 32px;
+  }
+}
+</style>
