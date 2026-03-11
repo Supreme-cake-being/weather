845c67e0f55b7b2f232760fda0af70b4f7a8d029
Kirill Korchakovskyi <121553762+Supreme-cake-being@users.noreply.github.com>
2026-03-11 23:00:15 +0200

    added container

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>Vite + Vue</h1>
+  <Container>
+    <h1>Vite + Vue</h1>
+  </Container>
 </template>
+
+<script setup lang="ts">
+import Container from "@/components/Container.vue";
+</script>
 
 <style scoped></style>
---
+++ src/components/Container.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.container {
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 var(--container-padding-sm);
+}
+
+@media (min-width: 576px) {
+  .container {
+    max-width: 540px;
+    padding: 0 var(--container-padding-md);
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 768px;
+  }
+}
+
+@media (min-width: 992px) {
+  .container {
+    max-width: 960px;
+    padding: 0 var(--container-padding-md);
+  }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1200px;
+    padding: 0 var(--container-padding-lg);
+  }
+}
+</style>
