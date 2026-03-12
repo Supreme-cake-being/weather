f5f801527dc9d09c5fc587b3cd1df7ee6206a12c
Kirill Korchakovskyi <121553762+Supreme-cake-being@users.noreply.github.com>
2026-03-12 13:13:05 +0200

    added weather card component

---
+++ src/constants/weatherIds.ts
@@ -0,0 +1,9 @@
+export const WEATHER_ID = {
+  THUNDERSTORM: { min: 200, max: 300 },
+  RAIN: { min: 300, max: 600 },
+  SNOW: { min: 600, max: 700 },
+  ATMOSPHERE: { min: 700, max: 800 },
+  CLEAR: 800,
+  FEW_CLOUDS: { min: 801, max: 803 },
+  OVERCAST: { min: 803, max: 805 },
+};
